ebadc375295ffcbd749ee09c84d651c384060a97
elizabeth sky degrate <skydegrate@gmail.com>
2022-07-01 20:25:42 -0400

    commit tree e6a162ee8f6e712d3ebd51e60295f437c86d8e1a

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(forTheHomies, event) {
+    let thankYouCards = []
+  for (let i = 0; i < forTheHomies.length; i++) {
+    thankYouCards.push(`Thank you, ${forTheHomies[i]}, for the wonderful ${event} gift!`)
+  }
+
+  return thankYouCards;
+}
+
+// for while loop
+
+function countDown(startWith) {
+    while (startWith > 0) {
+        console.log(startWith);
+        startWith -= 1;
+    }
+    console.log(startWith);
+}
+
